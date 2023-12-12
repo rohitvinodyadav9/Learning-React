8dfea84e5d2e0da5fd92382dba76db75dc6a6dd2
rohitvinodyadav9 <rohitvinodyadav9@gmail.com>
2023-12-13 00:06:51 +0530

    Final Commit

---
+++ 02-Card-Props/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card brand="macbook" tag1="#mac" tag2="#apple" btnText="Buy Now"/>
+      <Card brand="ideapad" tag1="#windows" tag2="#lenovo"/>
+    </>
+  )
+}
+
+export default App
